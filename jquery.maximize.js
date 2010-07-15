b2e4cc3c369ecf915cb46412337bdfee9e3faa87
Jarek Zmudzinski <jarek@toronegro.net>
2010-07-15 13:59:14 +0200

    zoom in limit, max width or height - work in progress

--- jquery.maximize.js
+++ jquery.maximize.js
@@ -24,7 +24,6 @@ jQuery(function ($) {
           function (x) { 
             return parseInt(img.css(x), 10); 
           }),
-        img_ratio,
         container_css = {},
         img_css = {},
         timer;
@@ -43,7 +42,9 @@ jQuery(function ($) {
         var w_h = $(window).height(),
           w_w = $(window).width(),
           w_ratio,
-          img_w, img_h;
+          img_w, 
+          img_h,
+          img_size = maximized.data('img_size');
 
         if (imgWrapper.length) {
           imgWrapper.height(w_h - padding[0] - padding[2]);
@@ -55,26 +56,35 @@ jQuery(function ($) {
         maximized.trigger('maximizerResize', [w_w, w_h]);
 
         if (config.resize === 'crop') {
-          if (w_ratio > img_ratio) {
+          if (w_ratio > img_size.ratio) {
             img_w = w_w;
-            img_h = w_w / img_ratio;
-            img.width(w_w).height(img_h);
+            img_h = w_w / img_size.ratio;
           } else {
-            img_w = w_h * img_ratio;
+            img_w = w_h * img_size.ratio;
             img_h = w_h;
-            img.height(w_h).width(img_w);
           }
         } else {
-          if (w_ratio < img_ratio) {
-            img_w = w_w;
-            img_h = w_w / img_ratio;
-            img.width(w_w - borders[1] - borders[3]).height(img_h - borders[0] - borders[2]);
+          if (w_ratio < img_size.ratio) {
+            img_w = w_w - borders[1] - borders[3];
+            if (config.zoomInLimit > 0 && img_w > img_size.width * config.zoomInLimit) {
+              img_w = img_size.width * config.zoomInLimit;
+            }
+            if (config.maxWidth > 0 && img_w > config.maxWidth) {
+              img_w = config.maxWidth;
+            }
+            img_h = (img_w / img_size.ratio) - borders[0] - borders[2];
           } else {
-            img_w = w_h * img_ratio;
-            img_h = w_h;
-            img.width(img_w - borders[1] - borders[3]).height(w_h - borders[0] - borders[2]);
+            img_h = w_h - borders[0] - borders[2];
+            if (config.zoomInLimit > 0 && img_h > img_size.height * config.zoomInLimit) {
+              img_h = img_size.height * config.zoomInLimit;
+            }
+            if (config.maxHeight > 0 && img_h > config.maxHeight) {
+              img_h = config.maxHeight;
+            }
+            img_w = (img_h * img_size.ratio) - borders[1] - borders[3];
           }
         }
+        img.width(img_w).height(img_h);
         if ($.inArray(config.center, ['both', 'horizontal']) !== -1) {
           img.css({left: (w_w - img_w) / 2});
         }
@@ -84,8 +94,12 @@ jQuery(function ($) {
       };
 
       timer = setInterval(function () {
+        var w, h, r;
         if (img[0].complete) {
-          img_ratio = img.width() / img.height();
+          w = img.width();
+          h = img.height();
+          r = w / h;
+          maximized.data('img_size', {width: w, height: h, ratio: r});
           resize();
           maximized.trigger('beforeShow');
           img.css({visibility: 'visible'}).fadeIn(500, 'swing');
@@ -102,7 +116,10 @@ jQuery(function ($) {
   $.fn.maximize.defaults = {
     center: 'both', //'horizontal','vertical'
     align: 'left',
-    resize: 'crop' // 'fill'
+    resize: 'crop', // 'fill'
+    zoomInLimit: 2, // works with {resize: 'fill'} only
+    maxWidth: 400, 
+    maxHeight: 0
   };
 
 });
--- photos.php
+++
@@ -1,3 +0,0 @@
-<?php
-  echo json_encode(explode("\n",shell_exec('curl -0 http://www.flickr.com/explore/interesting/7days/ | grep "pc_img" | sed s/.*src=\"// | sed s/\".*//')));
-?>
